325efbcd851764681f86dc770efb8d2c1191a9ea
Edwin Eulogio <e.huancachoque.yucra@gmail.com>
2026-05-27 17:49:11 -0500

    Primer commit del proyecto Generacion de Horarios

---
+++ src/main/java/com/utp/generacionhorarios/GeneracionDeHorariosApplication.java
@@ -0,0 +1,13 @@
+package com.utp.generacionhorarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneracionDeHorariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneracionDeHorariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utp/generacionhorarios/GeneracionDeHorariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.generacionhorarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneracionDeHorariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
